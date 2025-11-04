15a910877331bfbaab49f6341647f5eacba20407
kitykatgirl <kamilord220@vp.pl>
2025-11-04 09:01:50 +0100

    references to objects in mainactivity and color change on switch

--- app/src/main/java/ph/me/infokotor/MainActivity.java
+++ app/src/main/java/ph/me/infokotor/MainActivity.java
@@ -1,8 +1,15 @@
 package ph.me.infokotor;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,5 +27,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Switch switchColor = findViewById(R.id.switch1);
+        ImageView kotek = findViewById(R.id.imageCat);
+        Button prev = findViewById(R.id.buttonPrev);
+        Button next = findViewById(R.id.buttonNext);
+        EditText editText = findViewById(R.id.editTextKotek);
+        LinearLayout main = findViewById(R.id.main);
+
+        switchColor.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(@NonNull CompoundButton compoundButton, boolean b) {
+                        if (b){
+                            main.setBackgroundColor(getResources().getColor(R.color.butTheyreBlue));
+                        }
+                        else {
+                            main.setBackgroundColor(getResources().getColor(R.color.lighterGreen));
+                        }
+                    }
+                }
+        );
     }
 }
